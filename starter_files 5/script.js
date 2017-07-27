a5b84128551aa3a9db1d161cf605139c1ecd95b3
hamilton moore <hamlmoor@gmail.com>
2017-07-27 14:18:16 -0500

    completed assignment

--- starter_files 5/script.js
+++ starter_files 5/script.js
@@ -8,30 +8,39 @@ The `test.js` file contains tests for all of the functions in this project. Do n
 /********************
 Use the following array to complete tasks 1-3
 *********************/
-let numbers = [ 1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6 ];
+let numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
 
 
 /*
 1. Return the 4th element in the "numbers" array
 */
-function getFourthNum(){
+function getFourthNum() {
     // Your answer here:
+    return numbers[3];
 }
 
 /*
 2. Iterate over the "numbers" array. Push any numbers less than 10 onto "smallNums". Return "smallNums".
 */
-function smallNums(){
+function smallNums() {
     let smallNums = [];
     // Your answer here:
+    for (let i = 0; i < numbers.length; i++)
+        if (numbers[i] < 10) {
+            smallNums.push(numbers[i]);
+        }
+    return smallNums;
 }
 
 /*
 3. Add 12, 99, and 101 (in that order) to the end of the "numbers" array. Return the "numbers" array.
 */
-function addNums(){
+function addNums() {
     // Your answer here:
+    numbers.push(12, 99, 101);
+    return numbers;
 }
+
 
 
 
@@ -40,34 +49,40 @@ Use the following object to complete tasks 4-6
 *********************/
 
 let film = {
-    title : "Seven Samurai",
-    director : "Akira Kurosawa",
-    released : 1956,
-    runtime : 207,
-    budget : 2000000,
-    actors : [ "Toshiro Mifune", "Takashi Shimura", "Keiko Tsushima" ],
+    title: "Seven Samurai",
+    director: "Akira Kurosawa",
+    released: 1956,
+    runtime: 207,
+    budget: 2000000,
+    actors: ["Toshiro Mifune", "Takashi Shimura", "Keiko Tsushima"],
 };
 
 
 /*
 4. Add a property "boxoffice" with a value of 269061 to the "film" object. Return "film".
 */
-function addBoxOffice(){
+function addBoxOffice() {
     // Your answer here:
+    film.boxoffice = 269061;
+    return film;
 }
 
 /*
 5. Add the name "Yukiko Shimazaki" to the "actors" array. Return "film";
 */
-function addActor(){
+function addActor() {
     // Your answer here:
+    film.actors.push("Yukiko Shimazaki");
+    return film;
 }
 
 /*
 6. Now that you've added a "boxoffice" property, subtract "budget" from "property" and return the difference. This number is the amount lost by the studio in making the film. Return the loss.
 */
-function getLosses(){
+function getLosses() {
     // Your answer here:
+    let loss = film.boxoffice - film.budget;
+    return loss;
 }
 
 
@@ -81,12 +96,17 @@ function getLosses(){
 Your function should return [ "v5", "x67", "r34", "f456", "p78" ];
 */
 
-let numberVals = [ 5, 67, 34, 456, 78 ];
-let letterVals = [ "v", "x", "r", "f", "p" ];
+let numberVals = [5, 67, 34, 456, 78];
+let letterVals = ["v", "x", "r", "f", "p"];
 
-function interleave(){
+function interleave() {
     let vals = [];
     // Your answer here:
+    for (i = 0; i < numberVals.length; i++) {
+        // vals[i] = letterVals[i] + numberVals[i];
+        vals.push(letterVals[i] + numberVals[i]);
+    }
+    return vals;
 }
 
 
@@ -98,10 +118,16 @@ function interleave(){
 8. Iterate over the "first" and "second" arrays. Compare the values for both arrays. If the arrays values are the same, then store that value in the "same" array. Return "same".
 */
 
-let first = [ "blink", "stand", "glasses", "chair", "numinous", "adjacent", "bracelet", "hand" ];
-let second = [ "think", "stand", "cheese", "break", "numinous", "mouse", "close", "toe" ];
+let first = ["blink", "stand", "glasses", "chair", "numinous", "adjacent", "bracelet", "hand"];
+let second = ["think", "stand", "cheese", "break", "numinous", "mouse", "close", "toe"];
 
-function union(){
+function union() {
     let same = [];
     // Your answer here:
+    for (let i = 0; i < first.length; i++) {
+        if (first[i] === second[i]) {
+            same.push(first[i]);
+        }
+    }
+    return same;
 }
